bb6337238e728b8db74d57eafb28d2cede5d2bfd
Kevin Masseix <masseix.kevin@gmail.com>
2023-12-31 12:16:10 +0100

    add missing task wait_for_downstream_import

---
+++ task_functions/wait_for_downstream_import.py
@@ -0,0 +1,37 @@
+from dag_datalake_sirene.config import (
+    ELASTIC_DOWNSTREAM_URLS,
+    ELASTIC_DOWNSTREAM_USER,
+    ELASTIC_DOWNSTREAM_PASSWORD,
+    ELASTIC_DOWNSTREAM_ALIAS,
+)
+
+from datetime import datetime
+
+def wait_for_downstream_elastic_siren_import(**kwargs):
+    next_color = kwargs["ti"].xcom_pull(key="next_color", task_ids="get_colors")
+    elastic_index = f"siren-{next_color}"
+
+    downstream_urls = ELASTIC_DOWNSTREAM_URLS.split(',')
+
+    if len(downstream_urls) == 0:
+        return
+
+    pending = downstream_urls
+    completed = []
+
+    while len(pending) > 0:
+        for url in pending:
+
+            response = requests.get(
+                f'{ url }/{ELASTIC_DOWNSTREAM_ALIAS}',
+                auth=(ELASTIC_DOWNSTREAM_USER, ELASTIC_DOWNSTREAM_PASSWORD))
+
+            if response.status_code == 404:
+                continue
+
+            indices = list(response.json().keys())
+
+            if elastic_index in indices:
+                completed.append(url)
+
+        pending = [ url for downstream_urls if url not in completed ]
