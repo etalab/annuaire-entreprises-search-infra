f66e788998c2c75fd96fa9ae9336cd119c6a0ecd
Kevin Masseix <masseix.kevin@gmail.com>
2024-01-03 00:26:41 +0100

    feat: DAG to snapshot the next siren index and wait for it to be imported

--- helpers/get_colors.py
+++ helpers/get_colors.py
@@ -2,9 +2,13 @@ import json
 import logging
 from urllib.request import urlopen
 from dag_datalake_sirene.config import COLOR_URL
+from datetime import datetime
 
 
 def get_next_color():
+    return "20240102"
+    return datetime.today().strftime("%Y%m%d")
+
     try:
         with urlopen(COLOR_URL, timeout=5) as url:
             data = json.loads(url.read().decode())
@@ -27,7 +31,7 @@ def get_colors(**kwargs):
             current_color = data["CURRENT_COLOR"]
             logging.info(f"******************** Color file URL: {COLOR_URL}")
             logging.info(f"******************** Next color from file: {next_color}")
-            kwargs["ti"].xcom_push(key="next_color", value=next_color)
+            kwargs["ti"].xcom_push(key="next_color", value=NEXT_COLOR)
             kwargs["ti"].xcom_push(key="current_color", value=current_color)
     except BaseException as error:
         raise Exception(f"******************** Ouuups Error: {error}")
